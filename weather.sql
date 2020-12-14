696956801b1f5e27cc90e17bd79609c6d8591223
井家胜 <j_jiasheng@qq.com>
2020-12-14 12:09:40 +0800

    添加README说明文件以及数据库数据脚本文件

---
+++ weather.sql
@@ -0,0 +1,3 @@
+INSERT INTO `weather` VALUES (1, 'Sydney', 'Light Rain', '<<22℃', '16℃<<', '==19℃==', '/images/f11.png');
+INSERT INTO `weather` VALUES (2, 'Melbourne', 'sunny', '<<19℃', '7℃<<', '==14℃==', '/images/f2.png');
+INSERT INTO `weather` VALUES (3, 'Brisbane', 'ThounderShowers', '<<29℃', '18℃<<', '==25℃==', '/images/f4.png');
